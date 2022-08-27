b6121ad6ffd787406f753d2bd4cdf8ca641103cd
ziveo <bziveo@gmail.com>
2022-08-27 17:09:48 -0400

    Initial commit

---
+++ assets/jsonContext.3563f828.js
@@ -0,0 +1,1 @@
+import{bF as r,bE as e}from"./index.a2b594ae.js";function l(t){return{origin:"portal-item",url:r(t.itemUrl),portal:t.portal||e.getDefault(),portalItem:t,readResourcePaths:[]}}function a(t){return{origin:"portal-item",messages:[],writtenProperties:[],url:t.itemUrl?r(t.itemUrl):null,portal:t.portal||e.getDefault(),portalItem:t}}export{l as e,a as o};
